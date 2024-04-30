8c88f12ed74fccf22fb71146bc450062afe681cc
Bibek Dhungana <bibekdhungana121@gmail.com>
2024-04-30 14:22:42 +0545

    Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 
+
 function App() {
 
 
   return (
     <>
 
-      <div className="bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Mobile-Global.jpg')] lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Desktop-Global.png')] h-screen bg-cover bg-center">
+      <div >
         <Navbar />
-        <Home />
+        <div>
+          <Home />
+        </div>
+        <div>
+          <Home />
+        </div>
       </div>
-      
+
+
 
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <div className=''>
-        <div className='absolute inset-x-0 lg:top-[17%] sm:top-1  text-center'>
-            <h1 className='text-3xl font-bold'>Model Y</h1>
-            <h2 className='tezt-2xl font-bold p-1'>From $29,490</h2>
-            <p className='text-xs'>After Federal Tax Credit & Est Gas Saving</p>
+      <div className='relative w-full max-wi-lg'>
+        <img className="h-screen w-full object-cover" src="model-y.jpg" alt="Model Y" />
+        <div className='absolute top-10 left-0 right-0 bottom-0  items-center justify-center p-4'>
+          <h1 className="text-black text-3xl font-bold text-center ">Model Y</h1>
+          <h2 className='tezt-2xl font-bold p-1 text-center'>From $29,490</h2>
+          <p className='text-xs text-center'>After Federal Tax Credit & Est Gas Saving</p>
         </div>
-        <div className='flex flex-col lg:flex-row items-center justify-center absolute inset-x-0 top-[80%] text-center gap-4'>
-            <button className='bg-white rounded-md w-60 h-8 gap-3'>Order Now</button>
-            <button className='bg-black opacity-65 rounded-md w-60 h-8 text-white'>Demo Drive</button>
-        </div>
-        <p className="absolute inset-x-0 lg:top-[88%] sm:bottom-1 text-center text-xs">*Price before incentives is $42990, excluding taxes and fees. Subject to Change.</p>
+        <div class="absolute inset-x-0 bottom-10 flex justify-center items-center gap-4 px-9">
+        <button class="bg-white rounded-md w-60 h-15 text-black text-center font-medium shadow">Order Now</button>
+        <button class="bg-black opacity-75 rounded-md w-60 h-15 text-white text-center font-medium shadow">Demo Drive</button>
     </div>
+        <p className="absolute inset-x-0 px-20 bottom-5 text-center text-xs">*Price before incentives is $42990, excluding taxes and fees. Subject to Change.</p>
+      </div>
     </>
   )
 }
